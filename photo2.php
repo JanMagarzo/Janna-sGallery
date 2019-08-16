7a0b44c8aacbc27795980bff7e2bee9346eca69e
JanMagarzo <53500052+JanMagarzo@users.noreply.github.com>
2019-08-16 20:47:59 +0800

    Update and rename photo2.html to photo2.php

--- photo2.php
+++ photo2.php
@@ -4,8 +4,9 @@
 <body>
    <center><h1>Magarzo's Photo Gallery</h1></center>
     <center><p><img src="https://6.viki.io/image/ff72f69842af4bb484e01c6a61216bdb.jpeg?s=900x600&e=t" height ="150" width="300"></p>
-   <p><a href="photogalWeb.html">Back To Home</a> 
-   <a href="photo3.html">Next Photo</a> <p>
+   <p><a href="mygallery.php">Back To Home</a> 
+   <a href="photo3.html">Next Photo</a> <p>
+
     </center>
 
 
