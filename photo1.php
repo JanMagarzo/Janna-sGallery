30b54a14a61e4ecdf1fd47e2666dd20b83e372bd
JanMagarzo <53500052+JanMagarzo@users.noreply.github.com>
2019-08-16 20:46:58 +0800

    Update and rename gallery.html to photo1.php

--- photo1.php
+++ photo1.php
@@ -4,11 +4,10 @@
 <body>
    <center><h1>Magarzo's Photo Gallery</h1></center>
     <center><p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/180716_Seveteen_performing_%22Oh_My%21%22_at_You_Make_My_Day_Showcase.png/1024px-180716_Seveteen_performing_%22Oh_My%21%22_at_You_Make_My_Day_Showcase.png" height ="150" width="300"></p>
-   <p><a href="photogalWeb.html">Back To Home</a> 
-   <a href="photo2.html">Next Photo</a> <p>
+   <p><a href="mygallery.php">Back To Home</a> 
+   <a href="photo2.php">Next Photo</a> <p>
+
     </center>
-
-
 
 
 </body>
