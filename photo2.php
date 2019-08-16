6437911e47df1debb3a45af723a743e29a92e298
JanMagarzo <53500052+JanMagarzo@users.noreply.github.com>
2019-08-16 21:00:05 +0800

    Update photo2.php

--- photo2.php
+++ photo2.php
@@ -5,7 +5,7 @@
    <center><h1>Magarzo's Photo Gallery</h1></center>
     <center><p><img src="https://6.viki.io/image/ff72f69842af4bb484e01c6a61216bdb.jpeg?s=900x600&e=t" height ="150" width="300"></p>
    <p><a href="mygallery.php">Back To Home</a> 
-   <a href="photo3.html">Next Photo</a> <p>
+   <a href="photo3.php">Next Photo</a> <p>
 
     </center>
 
